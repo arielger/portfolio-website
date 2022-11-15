e0c33821de9db874076393a90902bec2004eb652
Ariel Gerstein <ariel.gers@gmail.com>
2022-11-14 23:45:58 -0300

    add initial home styles
    - update next to v13
    - delete commitlint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,38 @@
 import Head from 'next/head';
-import Image from 'next/image';
+import localFont from '@next/font/local';
 
 import styles from '@/styles/Home.module.css';
 
+const redaction100 = localFont({
+  src: '../styles/fonts/Redaction_100-Italic.woff2',
+});
+const redaction70 = localFont({
+  src: '../styles/fonts/Redaction_70-Italic.woff2',
+});
+
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>TypeScript starter for Next.js</title>
+        <title>({'>'}‿◠)✌ ariel gerstein / developer</title>
         <meta
           name="description"
-          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
+          content="Javascript developer specialized in React and Typescript"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <div className={styles.container}>
+        <h1 className={`${styles.title} ${redaction100.className}`}>
+          Ariel Gerstein
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{` `}
-          <code className={styles.code}>src/pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=typescript-nextjs-starter"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=typescript-nextjs-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{` `}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <h2 className={`${styles.subtitle} ${redaction70.className}`}>human</h2>
+        <h2 className={`${styles.subtitle} ${redaction70.className}`}>
+          developer
+        </h2>
+        <h2 className={`${styles.subtitle} ${redaction70.className}`}>
+          trying to build something cool
+        </h2>
+      </div>
     </div>
   );
 }
