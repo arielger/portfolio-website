7e0026841c37d2b3c9d4b11f851f5e6bf5d486d0
Ariel Gerstein <ariel.gers@gmail.com>
2023-05-30 00:52:55 -0300

    fix tina cms env variables

--- tina/config.ts
+++ tina/config.ts
@@ -4,8 +4,8 @@ const branch = process.env.VERCEL_GIT_COMMIT_REF || 'main';
 
 export default defineConfig({
   branch,
-  clientId: process.env.TINA_READ_ONLY_TOKEN ?? '', // Get this from tina.io
-  token: process.env.TINA_CLIENT_ID ?? '', // Get this from tina.io
+  clientId: process.env.TINA_CLIENT_ID ?? '', // Get this from tina.io
+  token: process.env.TINA_READ_ONLY_TOKEN ?? '', // Get this from tina.io
 
   build: {
     outputFolder: 'admin',
